6bb022a742fe5b7d78d1dd4cde6603eadae0ce8a
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-12-07 11:57:38 +0530

    Fized tailwind css

--- client/app/index.js
+++ client/app/index.js
@@ -1,14 +1,14 @@
 
-import React from 'react';
-import Navigation from '../navigation';
+// import React from 'react';
+// import Navigation from '../navigation';
 
 
 
 
-export default function App() {
-  return (
-    <Navigation/>
+// export default function App() {
+//   return (
+//     <Navigation/>
    
 
-  );
-}
+//   );
+// }
